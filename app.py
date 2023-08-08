117856785dd2bfed95a9b9bd4ff8a10a26badf48
ge59tas <julius.kasiske@tum.de>
2023-08-08 12:02:13 +0200

    finalized first iteration of app routing

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
 from flask import Flask
+import model
+import datetime
 
 app = Flask(__name__)
 
-@app.route("/<var>")
-def homepage(var):
-    return f"hello {var}, good to see you"
+def fill_values():
+    weekday = datetime.date.today().weekday()
+    day = 20200505
+    month = 5
+    return weekday, day, month
+
+@app.route("/<avgTemp>-<highTemp>-<lowTemp>-<precipitation>-<windSpeed>-<sunMinutes>")
+def homepage(avgTemp, highTemp, lowTemp, precipitation, windSpeed, sunMinutes):
+    weekday, day, month = fill_values()
+    prediction = model.predict_single(weekday, day, avgTemp, lowTemp, highTemp, precipitation, windSpeed, sunMinutes)
+    return_day = day[:4] + "." + day[4:6] + "." + day[6:]
+    return f"Expected gross profit for Best Bite on {return_day} will be: {prediction}"
 
 if (__name__) == "__main__":
+    # prepare model to make preds
+    model = model.BestBiteModel()
+    model.preprocess()
+    model.train(show_score=True)
     app.run(host="0.0.0.0")
